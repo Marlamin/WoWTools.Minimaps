58f96945c582b8f36ca54597cce9669558a643b8
Martin Benjamins <marlamin@marlamin.com>
2019-08-25 19:06:46 +0200

    Add project files.

---
+++ WoWTools.MinimapExtract/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WoWTools.MinimapExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
