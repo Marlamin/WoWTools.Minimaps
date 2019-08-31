6dc6946cc0704303b8d21678c21fc4ce1fe1f3c2
Martin Benjamins <marlamin@marlamin.com>
2019-08-31 14:20:52 +0200

    Implement TACT initialization, DBC reading

--- WoWTools.MinimapExtract/Program.cs
+++ WoWTools.MinimapExtract/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using DBCD.Providers;
+using System;
+using System.IO;
+using TACT.Net;
+using TACT.Net.Configs;
 
 namespace WoWTools.MinimapExtract
 {
@@ -6,7 +10,46 @@ namespace WoWTools.MinimapExtract
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var dir = Path.Combine("E:", "cdn");
+            var bc = "24cfe3b7a917e4b05c8a180ef8a13ae3";
+            var cdnc = "ba42282b283c46f43b96dc4c3465f321";
+
+            // Open storage for specific build
+            TACTRepo tactRepo = new TACTRepo(dir)
+            {
+                ConfigContainer = new ConfigContainer("wowt", Locale.US)
+            };
+
+            Console.WriteLine("Loading configs..");
+            tactRepo.ConfigContainer.OpenConfigs(dir, bc, cdnc);
+
+            Console.WriteLine("Loading indices..");
+            tactRepo.IndexContainer = new TACT.Net.Indices.IndexContainer();
+            tactRepo.IndexContainer.Open(dir);
+
+            Console.WriteLine("Loading encoding..");
+            tactRepo.EncodingFile = new TACT.Net.Encoding.EncodingFile(tactRepo.BaseDirectory, tactRepo.ConfigContainer.EncodingEKey);
+
+            Console.WriteLine("Looking up root..");
+            tactRepo.EncodingFile.TryGetCKeyEntry(tactRepo.ConfigContainer.RootCKey, out var rootCEntry);
+
+            Console.WriteLine("Loading root..");
+            tactRepo.RootFile = new TACT.Net.Root.RootFile(tactRepo.BaseDirectory, rootCEntry.EKey);
+
+            // Set up DBCD
+            var dbdProvider = new GithubDBDProvider();
+            var dbcProvider = new TACTDBCProvider(tactRepo);
+            var dbcd = new DBCD.DBCD(dbcProvider, dbdProvider);
+             
+            // Open Map DBC with local defs
+            Console.WriteLine("Opening Map.db2..");
+
+            // This will currently crash on partially encrypted DBs with unknown keys due to https://github.com/wowdev/TACT.Net/issues/12
+            var mapdb = dbcd.Load("Map");
+
+            // Run through maps/WDTs, generate filenames
+
+            // Append any filenames from listfile for additional non-WDT referenced minimaps?
         }
     }
 }
---
+++ WoWTools.MinimapExtract/TACTDBCProvider.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using TACT.Net;
+
+namespace DBCD.Providers
+{
+    class TACTDBCProvider : IDBCProvider
+    {
+        private TACTRepo tactRepo;
+
+        public TACTDBCProvider(TACTRepo tactRepo) 
+        {
+            this.tactRepo = tactRepo;
+        }
+
+        public Stream StreamForTableName(string tableName, string build)
+        {
+            uint fileDataID = 0;
+
+            // Replace this for Listfile.csv load at one point or something? Would be nice if DBCD supported loading things by FileDataID at one point but not sure how that'd work definition wise...
+            switch (tableName)
+            {
+                case "Map":
+                    fileDataID = 1349477;
+                    break;
+                default:
+                    throw new Exception("Don't know FileDataID for DBC " + tableName + ", add to switch please or implement listfile.csv reading. <3");
+            }
+
+            var stream = tactRepo.RootFile.OpenFile(fileDataID, tactRepo);
+            if(stream == null)
+            {
+                throw new Exception("Unable to open file with fileDataID " + fileDataID);
+            }
+
+            return stream;
+        }
+    }
+}
