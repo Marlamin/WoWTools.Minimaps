2f94a5967b166a7e0373958c105680add7bc3f41
Martin Benjamins <marlamin@marlamin.com>
2023-04-18 14:47:58 +0200

    Migrate extraction to CascLib

--- WoWTools.MinimapExtract/CASCDBCProvider.cs
+++ WoWTools.MinimapExtract/CASCDBCProvider.cs
@@ -1,23 +1,15 @@
 ﻿using System;
 using System.IO;
-using TACT.Net;
+using WoWTools.MinimapExtract;
 
 namespace DBCD.Providers
 {
-    class TACTDBCProvider : IDBCProvider
+    class CASCDBCProvider : IDBCProvider
     {
-        private TACTRepo tactRepo;
-
-        public TACTDBCProvider(TACTRepo tactRepo)
-        {
-            this.tactRepo = tactRepo;
-        }
-
         public Stream StreamForTableName(string tableName, string build)
         {
-            uint fileDataID = 0;
+            int fileDataID = 0;
 
-            // Replace this for Listfile.csv load at one point or something? Would be nice if DBCD supported loading things by FileDataID at one point but not sure how that'd work definition wise...
             switch (tableName)
             {
                 case "Map":
@@ -27,7 +19,7 @@ namespace DBCD.Providers
                     throw new Exception("Don't know FileDataID for DBC " + tableName + ", add to switch please or implement listfile.csv reading. <3");
             }
 
-            var stream = tactRepo.RootFile.OpenFile(fileDataID, tactRepo);
+            var stream = Program.cascHandler.OpenFile(fileDataID);
             if (stream == null)
             {
                 throw new Exception("Unable to open file with fileDataID " + fileDataID);
--- WoWTools.MinimapExtract/Program.cs
+++ WoWTools.MinimapExtract/Program.cs
@@ -1,85 +1,185 @@
-﻿using DBCD.Providers;
+﻿using CASCLib;
+using DBCD.Providers;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
-using TACT.Net;
-using TACT.Net.Configs;
 
 namespace WoWTools.MinimapExtract
 {
     class Program
     {
+        public static Dictionary<int, string> Listfile = new();
+        public static CASCHandler cascHandler;
         static void Main(string[] args)
         {
-            if (args.Length < 4)
+            if (args.Length < 2)
             {
-                throw new ArgumentException("Required arguments: cdndir, buildconfig, cdnconfig, outdir");
+                throw new ArgumentException("Required arguments: wowProduct outdir (wowPath)");
             }
 
-            var cdndir = args[0];
-            var bc = args[1];
-            var cdnc = args[2];
-            var outdir = args[3];
+            var wowProduct = args[0];
+            var outdir = args[1];
+
+            string wowPath = null;
+            if (args.Length > 2)
+            {
+                wowPath = args[2];
+            }
 
             var mapFilter = "";
-            if (args.Length == 5)
+            if (args.Length == 4)
             {
-                mapFilter = args[4];
+                mapFilter = args[3];
             }
 
-            // Open storage for specific build
-            TACTRepo tactRepo = new TACTRepo(cdndir)
+            CASCConfig.LoadFlags &= ~(LoadFlags.Download | LoadFlags.Install);
+            CASCConfig.ValidateData = false;
+            CASCConfig.ThrowOnFileNotFound = false;
+
+            if (wowPath == null)
             {
-                ConfigContainer = new ConfigContainer()
-            };
+                Console.WriteLine("Initializing CASC from web for program " + wowProduct);
+                cascHandler = CASCHandler.OpenOnlineStorage(wowProduct, "eu");
+            }
+            else
+            {
+                wowPath = wowPath.Replace("_retail_", "").Replace("_ptr_", "");
+                Console.WriteLine("Initializing CASC from local disk with basedir " + wowPath + " and program " + wowProduct);
+                cascHandler = CASCHandler.OpenLocalStorage(wowPath, wowProduct);
+            }
 
-            Console.WriteLine("Loading configs..");
-            tactRepo.ConfigContainer.OpenLocal(tactRepo.BaseDirectory, bc, cdnc);
+            var splitName = cascHandler.Config.BuildName.Replace("WOW-", "").Split("patch");
+            var buildName = splitName[1].Split("_")[0] + "." + splitName[0];
 
-            Console.WriteLine("Loading indices..");
-            tactRepo.IndexContainer = new TACT.Net.Indices.IndexContainer();
-            tactRepo.IndexContainer.Open(tactRepo.BaseDirectory, tactRepo.ConfigContainer);
+            cascHandler.Root.SetFlags(LocaleFlags.enUS);
 
-            Console.WriteLine("Loading encoding..");
-            tactRepo.EncodingFile = new TACT.Net.Encoding.EncodingFile(tactRepo.BaseDirectory, tactRepo.ConfigContainer.EncodingEKey);
-
-            Console.WriteLine("Looking up root..");
-            tactRepo.EncodingFile.TryGetCKeyEntry(tactRepo.ConfigContainer.RootCKey, out var rootCEntry);
-
-            Console.WriteLine("Loading root..");
-            tactRepo.RootFile = new TACT.Net.Root.RootFile(tactRepo.BaseDirectory, rootCEntry.EKey);
+            var availableFDIDs = new HashSet<int>();
+            
+            if (cascHandler.Root is WowTVFSRootHandler wtrh)
+                availableFDIDs = wtrh.RootEntries.Keys.ToHashSet();
+            else if (cascHandler.Root is WowRootHandler wrh)
+                availableFDIDs = wrh.RootEntries.Keys.ToHashSet();
 
             Console.WriteLine("Loading TACT keys..");
             try
             {
-                using (var client = new HttpClient())
+                var downloadKeys = false;
+                if (File.Exists("TactKey.csv"))
                 {
-                    var result = client.GetAsync("https://wow.tools/api.php?type=tactkeys").Result;
-                    var keys = result.Content.ReadAsStringAsync().Result.Split('\n');
-                    foreach(var key in keys)
+                    var info = new FileInfo("TactKey.csv");
+                    if (info.Length == 0 || DateTime.Now.Subtract(TimeSpan.FromDays(1)) > info.LastWriteTime)
                     {
-                        var split = key.Split(' ');
-                        if (split.Length < 2)
-                            continue;
-
-                        var keyname = Convert.ToUInt64(split[0], 16);
-                        var keybytes = split[1];
-                        if (TACT.Net.Cryptography.KeyService.TryAddKey(keyname, keybytes))
-                        {
-                            Console.WriteLine("Added TACT key " + split[0]);
-                        }
+                        Console.WriteLine("TACT Keys outdated, redownloading..");
+                        downloadKeys = true;
                     }
                 }
+                else
+                {
+                    downloadKeys = true;
+                }
+
+                if (downloadKeys)
+                {
+                    Console.WriteLine("Downloading TACT keys");
+
+                    List<string> tactKeyLines = new();
+                    using (var w = new HttpClient())
+                    using (var s = w.GetStreamAsync("https://github.com/wowdev/TACTKeys/raw/master/WoW.txt").Result)
+                    using (var sr = new StreamReader(s))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            var line = sr.ReadLine();
+                            if (string.IsNullOrEmpty(line))
+                                continue;
+
+                            var splitLine = line.Split(" ");
+                            tactKeyLines.Add(splitLine[0] + ";" + splitLine[1]);
+                        }
+                    }
+
+                    File.WriteAllLines("TactKey.csv", tactKeyLines);
+                }
+
+
+                foreach (var line in File.ReadAllLines("TactKey.csv"))
+                {
+                    var splitLine = line.Split(";");
+                    if (splitLine.Length != 2)
+                        continue;
+                }
+
+                KeyService.LoadKeys();
             }
             catch (Exception e)
             {
                 Console.WriteLine("An error occurred retrieving/loading TACT keys: " + e.Message);
             }
 
+            Console.WriteLine("Loading listfile..");
+            try
+            {
+                var downloadListfile = false;
+                if (File.Exists("listfile.csv"))
+                {
+                    var info = new FileInfo("listfile.csv");
+                    if (info.Length == 0 || DateTime.Now.Subtract(TimeSpan.FromDays(1)) > info.LastWriteTime)
+                    {
+                        Console.WriteLine("Listfile outdated, redownloading..");
+                        downloadListfile = true;
+                    }
+                }
+                else
+                {
+                    downloadListfile = true;
+                }
+
+                if (downloadListfile)
+                {
+                    Console.WriteLine("Downloading listfile");
+
+                    using (var w = new HttpClient())
+                    using (var s = w.GetStreamAsync("https://github.com/wowdev/wow-listfile/raw/master/community-listfile.csv").Result)
+                    {
+                        using var fs = new FileStream("listfile.csv", FileMode.OpenOrCreate);
+                        s.CopyTo(fs);
+                    }
+                }
+
+                if (!File.Exists("listfile.csv"))
+                {
+                    throw new FileNotFoundException("Could not find listfile.csv");
+                }
+
+                foreach (var line in File.ReadAllLines("listfile.csv"))
+                {
+                    if (string.IsNullOrEmpty(line))
+                        continue;
+
+                    var splitLine = line.Split(";");
+                    var fdid = int.Parse(splitLine[0]);
+
+                    if (!splitLine[1].StartsWith("world"))
+                        continue;
+
+                    if (!availableFDIDs.Contains(fdid))
+                        continue;
+                    
+                    if (splitLine[1].StartsWith("world/minimaps") || splitLine[1].EndsWith(".wdt"))
+                        Listfile[fdid] = splitLine[1];
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred retrieving/loading listfile: " + e.Message);
+            }
+            
             // Set up DBCD
             var dbdProvider = new GithubDBDProvider();
-            var dbcProvider = new TACTDBCProvider(tactRepo);
+            var dbcProvider = new CASCDBCProvider();
             var dbcd = new DBCD.DBCD(dbcProvider, dbdProvider);
 
             // Open Map DBC with local defs
@@ -111,14 +211,14 @@ namespace WoWTools.MinimapExtract
                         continue;
                     }
 
-                    wdtStream = tactRepo.RootFile.OpenFile((uint)map.WdtFileDataID, tactRepo);
+                    wdtStream = cascHandler.OpenFile((int)map.WdtFileDataID);
                 }
                 catch (Microsoft.CSharp.RuntimeBinder.RuntimeBinderException e)
                 {
                     Console.ForegroundColor = ConsoleColor.Yellow;
                     Console.WriteLine("Older build detected: map record has no wdtFileDataID property");
                     Console.ResetColor();
-                    wdtStream = tactRepo.RootFile.OpenFile("world/maps/" + map.Directory + "/" + map.Directory + ".wdt", tactRepo);
+                    wdtStream = cascHandler.OpenFile("world/maps/" + map.Directory + "/" + map.Directory + ".wdt");
                 }
 
                 if (wdtStream == null)
@@ -138,7 +238,7 @@ namespace WoWTools.MinimapExtract
                         for (var y = 0; y < 64; y++)
                         {
                             string tileName = "world/minimaps/" + map.Directory + "/map" + x.ToString().PadLeft(2, '0') + "_" + y.ToString().PadLeft(2, '0') + ".blp";
-                            var minimapStream = tactRepo.RootFile.OpenFile(tileName, tactRepo);
+                            var minimapStream = cascHandler.OpenFile(tileName);
                             if (minimapStream == null)
                             {
                                 continue;
@@ -162,7 +262,7 @@ namespace WoWTools.MinimapExtract
                     minimapFDIDs = minimapFDIDs.Where(chunk => chunk.fileDataId != 0).ToArray();
                     foreach (var minimap in minimapFDIDs)
                     {
-                        var minimapStream = tactRepo.RootFile.OpenFile(minimap.fileDataId, tactRepo);
+                        var minimapStream = cascHandler.OpenFile((int)minimap.fileDataId);
                         if (minimapStream == null)
                         {
                             Console.ForegroundColor = ConsoleColor.Red;
